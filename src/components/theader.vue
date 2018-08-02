b8de1b839395c0e1bfe37f37b2d7fa83b181ef18
shuizhou1 <18814143516@163.com>
2018-08-02 13:09:20 +0800

    second v

--- src/components/theader.vue
+++ src/components/theader.vue
@@ -1,75 +1,95 @@
 <template>
-    <header class="tHeader">
-      <div class="location"><span>广州</span></div>
-      <div class="logo"></div>
-      <div class="search"></div>
-    </header>
+  <header class="tHeader">
+    <div class="location"><span>广州</span></div>
+    <div class="logo"></div>
+    <div class="search">
+      <i class="fa fa-search"></i>
+    </div>
+  </header>
 </template>
 
 <script>
-    export default {
-        name: "tfooter",
-        data() {
-          return {
-            city: null,
-            locationId: null
+  export default {
+    name: "tfooter",
+    data() {
+      return {
+        city: null,
+        locationId: null
+      }
+    },
+    methods : {
+      async getLocationId() {
+        let self = this
+        // 百度地图api获取当前城市
+        this.$http.get('/baiduMap/location/ip', {
+          params: {
+            ak: 'm2xAiidueSdKT6ljTPFL3ZGTaTbbZHnm',
           }
-        },
-        methods : {
-          async getLocationId() {
-            let self = this
-            // 百度地图api获取当前城市
-            this.$http.get('/baiduMap/location/ip', {
-              params: {
-                ak: 'm2xAiidueSdKT6ljTPFL3ZGTaTbbZHnm',
-              }
-            })
-            .then(function(req){
-              let city = req.data.content.address_detail.city
-              // 截取字符串去掉最后一个‘市’字
-              self.city = city.substring(0,city.length-1)
-              // 根据得到的城市名与时光网的数据匹配得到对应的localtionId
-              self.$http.get('/api/Showtime/HotCitiesByCinema.api')
+        })
+          .then(function(req){
+            let city = req.data.content.address_detail.city
+            // 截取字符串去掉最后一个‘市’字
+            self.city = city.substring(0,city.length-1)
+            // 根据得到的城市名与时光网的数据匹配得到对应的localtionId
+            self.$http.get('/api/Showtime/HotCitiesByCinema.api')
               .then(function(req) {
                 let cityArr = req.data.p;
                 for(let val of cityArr) {
                   if(val.n === self.city) {
-                      self.$store.state.locationId = val.id
+                    self.$store.state.locationId = val.id
                     // self.locationId = val.id
                     console.log( self.$store.state.locationId, 111111)
                   }
                 }
               })
-            })
-            .catch(function(err) {
-              console.log(err);
-            })
-          },
-        },
-        created() {
-          this.getLocationId()
-        }
+          })
+          .catch(function(err) {
+            console.log(err);
+          })
+      },
+    },
+    created() {
+      this.getLocationId()
     }
+  }
 </script>
 
 <style lang="less" scoped>
   .tHeader{
     width: 100%;
-    height: 1rem;
+    height: 2rem;
     background: #222222;
     position: fixed;
     display: flex;
     justify-content: space-around;
     align-items: center;
     div {
-        height: 0.5rem;
-        width: 1rem;
-        border: 1px solid #ff0000;
-        flex: 0 1 auto
-     }
+      /*border: 1px solid #ff0000;*/
+      flex: 0 1 auto;
+      text-align: center;
+    }
     .location {
+      display: inline-block;
+      /*padding: 2px 3px;*/
+      color: aliceblue;
+    }
+    .logo {
+      height: 1.5rem;
+      width: 5rem;
+      background: url("../comen/img/head_logo.png") no-repeat;
+      background-size: cover;
+    }
+    /*background-repeat: ;*/
+    .search {
+      /*width: 2rem;*/
+      height: 2rem;
+      i.fa-search {
+        color: aliceblue;
+        line-height: 2rem;
 
+      }
     }
   }
+
 
 </style>
--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import './setRem'
 import $ from 'jquery'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import 'font-awesome/css/font-awesome.min.css'
 import store from './store'
 import axios from 'axios'
 
